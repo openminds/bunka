0dd39668359a53bbd655101ed117e7c84c66af7b
Steven De Coeyer <steven@banteng.be>
2013-10-01 16:11:50 +0200

    Creates proof of concept

---
+++ lib/santoku.rb
@@ -0,0 +1,64 @@
+require 'colorize'
+require 'net/ssh'
+require 'peach'
+require 'ridley'
+require 'yaml'
+
+class Santoku
+  def self.run
+    output_stream = Array.new
+    failed_output_stream = Array.new
+    timeout_output_stream = Array.new
+
+    ridley.node.all.peach(7) do |node|
+      begin
+        timeout 5 do
+          Net::SSH.start(node.chef_id, 'root', paranoid: false, forward_agent: true) do |ssh|
+            output_stream.push ssh.exec!('ls /home/')
+          end
+        end
+          print '.'.green
+      rescue TimeoutError
+        timeout_output_stream.push "#{node.chef_id}: connection timed out"
+        print 'F'.red
+      rescue Exception => e
+        failed_output_stream.push "#{node.chef_id}: #{e.message}"
+        print 'F'.red
+      end
+    end
+    print "\n"
+
+    timeout_output_stream.each do |output|
+      puts output.red
+    end
+
+    failed_output_stream.each do |output|
+      puts output.red
+    end
+
+    puts "\n---------------------------------------\n"
+
+    puts "#{'Success'.green}: #{output_stream.count}"
+    puts "#{'Timed out'.yellow}: #{timeout_output_stream.count}"
+    puts "#{'Failed'.red}: #{failed_output_stream.count}"
+  end
+
+private
+  def self.knife_config
+    if ::File.exist?(File.expand_path("../knife.rb", __FILE__))
+      Ridley::Chef::Config.from_file(File.expand_path("../knife.rb", __FILE__))
+    elsif ::File.exist?("#{ENV['HOME']}/.chef/knife.rb")
+      Ridley::Chef::Config.from_file("#{ENV['HOME']}/.chef/knife.rb")
+    else
+      raise 'Could not find knife.rb in current directory or home '
+    end
+  end
+
+  def self.ridley
+    @ridley ||= ridley = Ridley.new(
+      server_url: knife_config.chef_server_url,
+      client_name: knife_config.node_name,
+      client_key: knife_config.client_key
+    )
+  end
+end
---
+++ santoku.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |spec|
+  spec.name = 'santoku'
+  spec.version = '0.0.0'
+  spec.executables << 'santoku'
+  spec.date = '2013-10-01'
+  spec.summary = "Parrallel ssh commands over chef servers with rspec-like output"
+  spec.description = "A gem to perform command over parrallel ssh connections on multiple chef serverspec. Output is rspec-like."
+  spec.authors = ["Steven De Coeyer"]
+  spec.email = 'steven@banteng.be'
+  spec.files = ["lib/santoku.rb"]
+  spec.homepage = 'https://github.com/zhann/santoku'
+  spec.license = 'MIT'
+
+  spec.add_dependency 'colorize'
+  spec.add_dependency 'peach'
+  spec.add_dependency 'ridley'
+end
