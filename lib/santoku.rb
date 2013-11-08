58522bdd2d57f24374f4501f87f6cec27e0f0291
Steven De Coeyer <steven@banteng.be>
2013-11-08 11:55:59 +0100

    Adds custom ssh_exec! method

--- lib/santoku.rb
+++ lib/santoku.rb
@@ -62,3 +62,33 @@ private
     )
   end
 end
+
+def ssh_exec!(ssh, command)
+  # I am not awesome enough to have made this method myself
+  # I've just modified it a bit
+  # Originally submitted by 'flitzwald' over here: http://stackoverflow.com/a/3386375
+  stdout_data = ""
+  stderr_data = ""
+  exit_code = nil
+
+  ssh.open_channel do |channel|
+    channel.exec(command) do |ch, success|
+      unless success
+        abort "FAILED: couldn't execute command (ssh.channel.exec)"
+      end
+      channel.on_data do |ch,data|
+        stdout_data+=data
+      end
+
+      channel.on_extended_data do |ch,type,data|
+        stderr_data+=data
+      end
+
+      channel.on_request("exit-status") do |ch,data|
+        exit_code = data.read_long
+      end
+    end
+  end
+  ssh.loop
+  [stdout_data, stderr_data, exit_code]
+end
