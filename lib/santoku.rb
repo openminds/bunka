04b31501a7c5029bb8792763fe8ff394a2e0be97
Steven De Coeyer <steven@banteng.be>
2013-11-17 15:37:14 +0100

    Updates lib/santoku to use workers

--- lib/santoku.rb
+++ lib/santoku.rb
@@ -1,112 +1,42 @@
-require 'colorize'
-require 'net/ssh'
 require 'peach'
-require 'ridley'
-require 'yaml'
+
+require 'santoku/helpers'
+require 'santoku/printers'
 
 class Santoku
-  def self.run(command='uptime', query)
-    output_stream = Array.new
-    failed_output_stream = Array.new
-    timeout_output_stream = Array.new
+  class << self
+    def test(command, query, timeout_interval, verbose_success, invert)
+      @verbose_success = verbose_success
+      @invert = invert
 
-    ridley.search(:node, query).peach(5) do |node|
-      begin
-        timeout 10 do
-          Net::SSH.start(node.chef_id, 'root', paranoid: false, forward_agent: true) do |ssh|
-            output = ssh_exec!(ssh, command)
-            if output[2] != 0
-              failed_output_stream.push "#{node.chef_id} error #{output[2]}: #{output[1]}"
-              print 'F'.red
-            else
-              output_stream.push "#{node.chef_id}: #{output[0]}"
-              print '.'.green
+      ridley.search(:node, query).peach(5) do |node|
+        begin
+          timeout timeout_interval do
+            Net::SSH.start(node.chef_id, 'root', paranoid: false, forward_agent: true) do |ssh|
+              output = ssh_exec!(ssh, command)
+              if output[2] == 0 && !invert?
+                succeeded "#{node.chef_id}: #{output[0]}"
+              elsif output[2] != 0 && invert?
+                succeeded "#{node.chef_id} returned #{output[2]}: #{output[1]} #{output[0]}"
+              else
+                failed "#{node.chef_id} returned #{output[2]}: #{output[1]} #{output[0]}"
+              end
             end
           end
+        rescue TimeoutError
+          timed_out "#{node.chef_id}: connection timed out"
+        rescue Errno::ETIMEDOUT
+          timed_out "#{node.chef_id}: Operation timed out - connect(2)"
+        rescue SocketError
+          timed_out "#{node.chef_id}: node does not resolve"
+        rescue Errno::EHOSTUNREACH
+          timed_out "#{node.chef_id}: no route to host"
+        rescue Exception => e
+          timed_out "#{node.chef_id}: #{e.inspect}"
         end
-
-      rescue TimeoutError
-        timeout_output_stream.push "#{node.chef_id}: connection timed out"
-        print '*'.yellow
-      rescue SocketError
-        timeout_output_stream.push "#{node.chef_id}: node does not resolve"
-        print '*'.yellow
-      rescue Errno::EHOSTUNREACH
-        timeout_output_stream.push "#{node.chef_id}: no route to host"
-        print '*'.yellow
-      rescue Exception => e
-        failed_output_stream.push "#{node.chef_id}: #{e.inspect}"
-        print 'F'.red
       end
-    end
 
-    print "\n"
-
-    timeout_output_stream.each do |output|
-      puts output.yellow
-    end
-
-    failed_output_stream.each do |output|
-      puts output.red
-    end
-
-    output_stream.each do |output|
-      puts output.green
-    end
-
-    puts "\n---------------------------------------\n"
-
-    puts "#{'Success'.green}: #{output_stream.count}"
-    puts "#{'Timed out or does not resolve'.yellow}: #{timeout_output_stream.count}"
-    puts "#{'Failed'.red}: #{failed_output_stream.count}"
-  end
-
-private
-  def self.knife_config
-    if ::File.exist?(File.expand_path("../knife.rb", __FILE__))
-      Ridley::Chef::Config.from_file(File.expand_path("../knife.rb", __FILE__))
-    elsif ::File.exist?("#{ENV['HOME']}/.chef/knife.rb")
-      Ridley::Chef::Config.from_file("#{ENV['HOME']}/.chef/knife.rb")
-    else
-      raise 'Could not find knife.rb in current directory or home '
+      print_summary
     end
   end
-
-  def self.ridley
-    @ridley ||= Ridley.new(
-      server_url: knife_config.chef_server_url,
-      client_name: knife_config.node_name,
-      client_key: knife_config.client_key
-    )
-  end
-end
-
-def ssh_exec!(ssh, command)
-  # I am not awesome enough to have made this method myself
-  # I've just modified it a bit
-  # Originally submitted by 'flitzwald' over here: http://stackoverflow.com/a/3386375
-  stdout_data = ""
-  stderr_data = ""
-  exit_code = nil
-
-  ssh.open_channel do |channel|
-    channel.exec(command) do |ch, success|
-      unless success
-        abort "FAILED: couldn't execute command (ssh.channel.exec)"
-      end
-      channel.on_data do |ch,data|
-        stdout_data+=data
-      end
-
-      channel.on_extended_data do |ch,type,data|
-        stderr_data+=data
-      end
-
-      channel.on_request("exit-status") do |ch,data|
-        exit_code = data.read_long
-      end
-    end
-  end
-  ssh.loop
-  [stdout_data, stderr_data, exit_code]
 end
